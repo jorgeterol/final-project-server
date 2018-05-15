bef6e7c16df339f88201d074ee219b3c1ea058d3
Jorge Terol <jorgetd92@gmail.com>
2018-05-15 15:13:58 +0200

    Fixed bug with torrentfinder

--- helpers/torrentfinder.js
+++ helpers/torrentfinder.js
@@ -2,14 +2,16 @@
 const axios = require('axios');
 
 const torrentFinder = function (imbdId) {
-  const url = `https://yts.am/api/v2/list_movies.json?quality=720&query_term=${imbdId}`;
+  const url = `https://yts.am/api/v2/list_movies.json?&query_term=${imbdId}`;
   return axios({
     method: 'get',
     url: url
   })
     .then((response) => {
-      console.log(response);
-      return response.data.data.movies[0].torrents;
+      if (response.data.data.movie_count === 0) {
+        return;
+      };
+      return response.data.data.movies;
     });
 };
 
