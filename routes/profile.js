85af4abcb14ada4bbdddd64d89fb22c652a9e92a
Jorge Terol <jorgetd92@gmail.com>
2018-05-17 10:27:09 +0200

    Deleting comments from the database

--- routes/profile.js
+++ routes/profile.js
@@ -2,9 +2,11 @@
 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const User = require('../models/user');
+const Comment = require('../models/comment');
 
-/* GET home page. */
+/* GET profile page. */
 router.get('/:username', (req, res, next) => {
   if (!req.session.currentUser) {
     res.status(401).json({ code: 'not-authorized' });
@@ -20,6 +22,31 @@ router.get('/:username', (req, res, next) => {
     .populate('comments.show')
     .then((user) => {
       res.json(user);
+    })
+    .catch(next);
+});
+
+router.delete('/:id', (req, res, next) => {
+  if (!req.session.currentUser) {
+    res.status(401).json({ code: 'not-authorized' });
+  }
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(422).json({ code: 'unprocessable-entity' });
+  }
+
+  const userId = req.session.currentUser._id;
+
+  Comment.findByIdAndRemove(req.params.id)
+    .then((comment) => {
+      if (!comment) {
+        res.json({code: 'comment-not-found'});
+        return;
+      }
+      comment.remove()
+        .then((comment) => {
+          res.json(comment);
+        })
+        .catch(next);
     })
     .catch(next);
 });
